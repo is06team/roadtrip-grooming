1933620c5926bfbaf81487f8d1e96dbd8b86f67b
Thomas Noury <thomas.noury@adevinta.com>
2021-04-14 14:48:09 +0200

    Fix memory leak by rerendering fontawesome inclusion on each typing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <script type="text/javascript" src="https://kit.fontawesome.com/c8a09f3a5c.js"></script>
+      </Head>
+    </>
+  )
 }
 
 export default MyApp
