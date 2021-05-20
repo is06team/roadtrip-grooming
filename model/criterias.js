0790793e71e5caad47285d0a7efd7f9fabcb7791
Thomas Noury <thomas.noury@adevinta.com>
2021-05-20 16:59:11 +0200

    Added criteria suggestions

---
+++ model/criterias.js
@@ -0,0 +1,9 @@
+export const criteriaDictionary = [
+    "GIVEN je suis un acheteur",
+    "GIVEN je suis un vendeur",
+    "WHEN je clique sur le bouton",
+    "WHEN je clique sur le lien",
+    "WHEN je vais sur la page",
+    "THEN je vois l'écran",
+    "THEN je vois un message d'erreur générique"
+]
--- pages/components/IncrementPhase/components/Increment/components/Criteria/index.js
+++ pages/components/IncrementPhase/components/Increment/components/Criteria/index.js
@@ -1,30 +1,60 @@
 import React, { useState } from 'react'
-
+import { criteriaDictionary as Suggestions } from '../../../../../../../model/criterias'
 import styles from './styles.module.scss'
 
 const Criteria = ({ id, title, gherkin, onChange, onDelete }) => {
-  /**
-   * Initial data
-   */
+
   const [data, setData] = useState({
     id: id,
     title: title,
     gherkin: gherkin,
   })
 
-  /**
-   * 
-   * @param {string} field 
-   * @param {string} value 
-   */
-  const updateField = (field, value) => {
-    setData({ ...data, [field]: value })
-    onChange({ ...data, [field]: value })
+  const [suggestion, setSuggestion] = useState('')
+
+  const getSuggestion = (text) => {
+    if (!text) return '';
+    const suggestions = Suggestions.filter((s) => {
+      return s.startsWith(text)
+    })
+    return suggestions[0] || ''
   }
 
-  /**
-   * Rendering
-   */
+  const getCurrentLine = (lines, position) => {
+    let i = 0
+    let charCount = lines[i].length
+    while (charCount < position && i !== lines.length - 1) {
+      charCount += lines[++i].length + 1
+    }
+    return i
+  }
+
+  const handleKeyPressed = (event) => {
+    if (event.which !== 13) {
+      return
+    }
+    setData({ ...data, ['gherkin']: suggestion })
+    onChange({ ...data, ['gherkin']: suggestion })
+  }
+
+  const updateGherkin = (selectionStart, value) => {
+    const lines = value.split('\n')
+    const currentLine = getCurrentLine(lines, selectionStart)
+    const sugg = getSuggestion(lines[currentLine])
+    setSuggestion(
+      lines.map((line, index) => {
+        return (index === currentLine ? sugg : line)
+      }).join('\n')
+    )
+    setData({ ...data, ['gherkin']: value })
+    onChange({ ...data, ['gherkin']: value })
+  }
+
+  const updateTitle = (event) => {
+    setData({ ...data, ['title']: event.target.value })
+    onChange({ ...data, ['title']: event.target.value })
+  }
+
   return (
     <div className="user_story_increment_criteria_item">
       <div className={styles.Criteria}>
@@ -34,16 +64,20 @@ const Criteria = ({ id, title, gherkin, onChange, onDelete }) => {
             className="user_story_increment_criteria_title"
             value={data.title}
             placeholder="Titre du critère"
-            onChange={(e) => updateField('title', e.target.value) } />
+            onChange={updateTitle} />
           <button className="criteriaTitle" onClick={() => onDelete(data.id) }>Supprimer</button>
         </div>
         <div className={styles.content}>
-          <textarea
-            className="user_story_increment_criteria_text"
-            placeholder="GIVEN WHEN THEN"
-            value={data.gherkin}
-            onChange={(e) => updateField('gherkin', e.target.value)}></textarea>
-        </div>        
+          <div className={styles.contentField}>
+            <div className={styles.contentSuggestion}>{suggestion}</div>
+            <textarea
+              className="user_story_increment_criteria_text"
+              placeholder="GIVEN WHEN THEN"
+              value={data.gherkin}
+              onChange={(e) => updateGherkin(e.target.selectionStart, e.target.value)}
+              onKeyPress={handleKeyPressed}></textarea>
+          </div>
+        </div>
       </div>
     </div>
   )
